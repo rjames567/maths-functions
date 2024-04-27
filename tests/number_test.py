b5e2ff2a40beae35085bff13e972a7a861118fad
Reuben James <123649904+rjames567@users.noreply.github.com>
2024-04-27 10:41:08 +0000

    Write decimal place rounding tests

--- tests/number_test.py
+++ tests/number_test.py
@@ -93,3 +93,32 @@ class TestRealNumber(unittest.TestCase):
 			Exception,
 			temp
 		)
+
+	def test_exact_value_getter(self):
+		n1 = number.RealNumber(1/3)
+		assert(n1.exact_value == (1/3))
+
+		n2 = number.RealNumber(2)
+		assert(n2.exact_value == 2)
+
+		n3 = number.RealNumber(numerator=3, denominator=4)
+		assert(n3.exact_value == (3/4))
+
+		n4 = number.RealNumber(4/5, as_fraction=False)
+		assert (n4.exact_value == (4/5))
+
+	def test_decimal_places_rounding(self):
+		n1 = number.RealNumber(1/3, decimal_places=3)
+		assert(n1.rounded_value == 0.333)
+
+		n2 = number.RealNumber(2, decimal_places=1)
+		assert(n2.rounded_value == 2)
+
+		n3 = number.RealNumber(numerator=3, denominator=4, decimal_places=1)
+		assert(n3.rounded_value == 0.8)
+
+		n4 = number.RealNumber(9/5, as_fraction=False, decimal_places=7)
+		assert (n4.rounded_value == 1.8)
+
+		n5 = number.RealNumber(2/3, as_fraction=False, decimal_places=7)
+		assert (n5.rounded_value == 0.6666667)
